e89bf96679c6138dd2b9ed385c7cc02a73231e4a
Gedev <gerald.devreux.pro@gmail.com>
2023-05-18 19:09:00 +0200

    [1.1] Stable release
    Create Homepage Component,
    Form and console.log response from backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,34 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {MatButtonModule} from "@angular/material/button";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatIconModule} from "@angular/material/icon";
+import { HomepageComponent } from './homepage/homepage.component';
+import {RouterOutlet} from "@angular/router";
+import { AppRoutingModule } from './app-routing.module';
+import {MatInputModule} from "@angular/material/input";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    RouterOutlet,
+    AppRoutingModule,
+    MatInputModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {NgForm} from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.css']
 })
-export class AppComponent {
+export class HomepageComponent {
+  title = 'Highlight Me';
   text: string | undefined;
   protected readonly onsubmit = onsubmit;
 
   constructor(private http: HttpClient) { }
 
-  submitForm() {
+  onSubmit(form: NgForm) {
+    console.log("Submit!")
     this.http.post('http://localhost:8000/api/', { text: this.text }).subscribe(response => {
       console.log(response);
     });
