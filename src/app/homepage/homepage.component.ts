2fb006a23d975bf0a52ed0cde7bcbf2106a8c639
Gedev <gerald.devreux.pro@gmail.com>
2023-05-20 01:18:14 +0200

    [1.3 Stable] Display table of players, no report message when wrong wl code

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -12,11 +12,12 @@ export class HomepageComponent {
   text: string | undefined;
   protected readonly onsubmit = onsubmit;
   response: any;
+
+
   constructor(private http: HttpClient) { }
 
   onSubmit(form: NgForm) {
     console.log("Submit!")
-    console.log(form)
     this.http.post('http://localhost:8000/api/', form.value).subscribe(data=> {
       this.response = data;
     });
